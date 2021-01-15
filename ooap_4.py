44149718671a22ea0fa2fc4c255d8eb8bc03414e
loser-1234 <76598368+loser-1234@users.noreply.github.com>
2021-01-15 17:36:49 +0300

    Create ooap_4.py

---
+++ ooap_4.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+    @abstractmethod
+    def undo(self) -> None:
+        pass
+        
+class NoCommand(Command):
+    def execute(self) -> None:
+        pass
+        
+class LightOnCommand(Command):
+    
+    def __init__(self, light: Light) -> None:
+        self._light = light
+
+    def execute(self) -> None:
+        self._light.on()
+        
+    def undo(self) -> None:
+        self._light.off()
+    
+class LightOffCommand(Command):
+    
+    def __init__(self, light: Light) -> None:
+        self._light = light
+
+    def execute(self) -> None:
+        self._light.off()
+        
+    def undo(self) -> None:
+        self._light.on()
+
+class Light: #получатель
+    
+    def on(self) -> None:
+        print("Light is on")
+        
+    def off(self) -> None:
+        print("Light is off")
+        
+class GarageDoorOpenCommand(Command):
+    
+    def __init__(self, garageDoor: GarageDoor) -> None:
+        self._garageDoor = garageDoor
+
+    def execute(self) -> None:
+        self._garageDoor.up()
+        
+    def undo(self) -> None:
+        self._garageDoor.down()
+        
+class GarageDoorCloseCommand(Command):
+    
+    def __init__(self, garageDoor: GarageDoor) -> None:
+        self._garageDoor = garageDoor
+
+    def execute(self) -> None:
+        self._garageDoor.down()        
+
+    def undo(self) -> None:
+        self._garageDoor.up()
+        
+class GarageDoor: #получатель
+    
+    def up(self) -> None:
+        print("Garage door is open")
+        
+    def down(self) -> None:
+        print("Garage door is close")
+class SimpleRemoteControl:
+    
+    def __init__(self):
+        self.commands = list()
+    
+    def execute(self, command):
+        command.execute()
+        self.commands.append(command)
+
+    def undo(self):
+        if (len(self.commands) != 0):
+            self.commands.pop().undo()
+        else:
+            print('Нет команд для отмены')
+            
+if __name__ == "__main__":
+    
+    remoteControl = SimpleRemoteControl()
+    light = Light()
+    garageDoor = GarageDoor()
+    remoteControl.execute(LightOffCommand(light))
+    remoteControl.execute(GarageDoorCloseCommand(garageDoor))
+    remoteControl.undo()
+    remoteControl.undo()
+    remoteControl.undo()
